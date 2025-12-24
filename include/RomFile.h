686d4fa847494f8957b14d40f77d328329e78556
Jakub Błaszczyk <72347694+liitlelenim@users.noreply.github.com>
2025-12-24 22:37:58 +0100

    Create RomFile class for loading ROM data from specified file path

---
+++ include/RomFile.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#include <filesystem>
+#include <vector>
+#include <memory>
+#include <fstream>
+
+class RomFile {
+public:
+    static constexpr size_t RomSizeInBytes = 3584;
+private:
+    std::vector<std::byte> memoryContent;
+public:
+    explicit RomFile(const std::filesystem::path &romPath);
+
+    [[nodiscard]] const std::vector<std::byte> &GetData() const;
+
+private:
+    void ReadFile(std::ifstream &romFile);
+
+};
---
+++ src/RomFile.cpp
@@ -0,0 +1,24 @@
+﻿#include "RomFile.h"
+#include <iostream>
+
+RomFile::RomFile(const std::filesystem::path &romPath) {
+    memoryContent.reserve(RomSizeInBytes);
+    std::ifstream romInputFileStream = std::ifstream(romPath, std::ios::binary);
+    ReadFile(romInputFileStream);
+}
+
+void RomFile::ReadFile(std::ifstream &romFile) {
+    if (!romFile) {
+        throw std::runtime_error("ROM file could not be opened");
+    }
+
+    int value;
+    while ((value = romFile.get()) != EOF) {
+        memoryContent.emplace_back(static_cast<std::byte>(value));
+    }
+}
+
+const std::vector<std::byte> &RomFile::GetData() const {
+    return memoryContent;
+}
+
