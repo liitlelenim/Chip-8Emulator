922a027e3efa4bf6207c2290ccf75cf1a0dcf094
Jakub Błaszczyk <72347694+liitlelenim@users.noreply.github.com>
2025-12-24 21:15:00 +0100

    Create basic CLI argument handling for locating rom file

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <filesystem>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+int main(int argc, char **argv) {
+
+    if (argc != 2) {
+        std::cerr << "Proper usage:\n" << argv[0] << " [rom-path]";
+        return EXIT_FAILURE;
+    }
+
+    std::filesystem::path romPath = argv[1];
+    if (!std::filesystem::exists(romPath)) {
+        std::cerr << "File not found: " << romPath;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
