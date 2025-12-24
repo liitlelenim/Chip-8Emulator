634b9c89ce94d6aedae8f377eaa0b0d3f8d3606e
Jakub Błaszczyk <72347694+liitlelenim@users.noreply.github.com>
2025-12-24 23:09:22 +0100

    Create base interpreter and functionality of copying rom data into the memory

---
+++ include/Interpreter.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#include <vector>
+#include <memory>
+#include <array>
+#include "RomFile.h"
+
+class Interpreter {
+public:
+    static constexpr size_t SizeOfMemoryInBytes = 0xFFF; //4096 bytes
+    static constexpr size_t ProgramStartAddress = 0x200; //512 for most programs
+public:
+    explicit Interpreter(const RomFile &romFile);
+
+private:
+    std::vector<std::byte> memory{SizeOfMemoryInBytes};
+    std::array<uint8_t, 16> Registers{0};
+    uint16_t programCounter{ProgramStartAddress};
+    uint8_t stackPointer{0};
+
+};
---
+++ src/Interpreter.cpp
@@ -0,0 +1,6 @@
+﻿#include "Interpreter.h"
+
+Interpreter::Interpreter(const RomFile &romFile) {
+    const std::vector<std::byte> &romData = romFile.GetData();
+    std::copy(romData.begin(), romData.end(), memory.begin() + ProgramStartAddress);
+}
