6d5fdbaf3fb9dcd3d592ebc886819ca778de3bbf
Jakub Błaszczyk <72347694+liitlelenim@users.noreply.github.com>
2025-12-25 23:11:44 +0100

    Create base for display window

---
+++ include/DisplayWindow.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#include "SFML/Graphics/RenderWindow.hpp"
+
+class DisplayWindow {
+public:
+    static constexpr unsigned int DisplayWidth = 64;
+    static constexpr unsigned int DisplayHeight = 64;
+    static constexpr unsigned int EmulationResolutionMultiplier = 10;
+private:
+    sf::RenderWindow window{sf::VideoMode({640, 320}), "CHIP-8"};
+public:
+    bool ShouldBeOpen() const;
+
+    void PollEvents();
+
+    void Draw();
+
+private:
+    void HandleEvent(const sf::Event &event);
+};
---
+++ src/DisplayWindow.cpp
@@ -0,0 +1,23 @@
+﻿#include "DisplayWindow.h"
+
+bool DisplayWindow::ShouldBeOpen() const {
+    return window.isOpen();
+}
+
+void DisplayWindow::PollEvents() {
+    while (const std::optional<sf::Event> event = window.pollEvent()) {
+        // while loop already makes sure that optional has value
+        HandleEvent(event.value());
+    }
+}
+
+void DisplayWindow::HandleEvent(const sf::Event &event) {
+    if (event.is<sf::Event::Closed>()) {
+        window.close();
+    }
+}
+
+void DisplayWindow::Draw() {
+    window.clear();
+    window.display();
+}
