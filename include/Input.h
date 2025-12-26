cc1c1efdcb16ad9b05f9a731228cfc5f944087b6
Jakub Błaszczyk <72347694+liitlelenim@users.noreply.github.com>
2025-12-26 22:29:45 +0100

    Create input class for mapping keyboard keys to adequate register values

---
+++ include/Input.h
@@ -0,0 +1,41 @@
+﻿#pragma once
+
+#include <array>
+#include <SFML/Window.hpp>
+
+/*
+ * We're mapping this chip-8 layout to
+ * qwerty keyboard
+    1 2	3 C
+    4 5	6 D
+    7 8 9 E
+    A 0	B F
+    =>
+    1 2 3 4
+    q w e r
+    a s d f
+    z x c v
+ */
+
+class Input {
+public:
+
+    static constexpr std::array<sf::Keyboard::Key, 16> ValueToKey{
+            sf::Keyboard::Key::X,
+            sf::Keyboard::Key::Num1,
+            sf::Keyboard::Key::Num2,
+            sf::Keyboard::Key::Num3,
+            sf::Keyboard::Key::Q,
+            sf::Keyboard::Key::W,
+            sf::Keyboard::Key::E,
+            sf::Keyboard::Key::A,
+            sf::Keyboard::Key::S,
+            sf::Keyboard::Key::D,
+            sf::Keyboard::Key::Z,
+            sf::Keyboard::Key::C,
+            sf::Keyboard::Key::Num4,
+            sf::Keyboard::Key::R,
+            sf::Keyboard::Key::F,
+            sf::Keyboard::Key::V
+    };
+};
