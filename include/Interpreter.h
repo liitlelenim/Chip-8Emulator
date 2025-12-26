491619fa39f2c4dc6c208c76fd557484d53a4837
Jakub Błaszczyk <72347694+liitlelenim@users.noreply.github.com>
2025-12-26 01:10:37 +0100

    Create scaffolding for instructions handling

--- include/Interpreter.h
+++ include/Interpreter.h
@@ -3,19 +3,63 @@
 #include <vector>
 #include <memory>
 #include <array>
+#include <unordered_map>
+#include <functional>
 #include "RomFile.h"
 
 class Interpreter {
 public:
     static constexpr size_t SizeOfMemoryInBytes = 0xFFF; //4096 bytes
     static constexpr size_t ProgramStartAddress = 0x200; //512 for most programs
-public:
-    explicit Interpreter(const RomFile &romFile);
-
 private:
     std::vector<std::byte> memory{SizeOfMemoryInBytes};
+
     std::array<uint8_t, 16> Registers{0};
     uint16_t programCounter{ProgramStartAddress};
     uint8_t stackPointer{0};
+
+    std::unordered_map<uint8_t, std::function<void(uint16_t opCode)>> instructionsMethods;
+
+public:
+    explicit Interpreter(const RomFile &romFile);
+
+    void PerformCurrentInstruction();
+
+private:
+    [[nodiscard]] uint16_t GetCurrentInstruction() const;
+
+    void InitializeMethodInstructions();
+
+    void OP_0(uint16_t opCode);
+
+    void OP_1(uint16_t opCode);
+
+    void OP_2(uint16_t opCode);
+
+    void OP_3(uint16_t opCode);
+
+    void OP_4(uint16_t opCode);
+
+    void OP_5(uint16_t opCode);
+
+    void OP_6(uint16_t opCode);
+
+    void OP_7(uint16_t opCode);
+
+    void OP_8(uint16_t opCode);
+
+    void OP_9(uint16_t opCode);
+
+    void OP_A(uint16_t opCode);
+
+    void OP_B(uint16_t opCode);
+
+    void OP_C(uint16_t opCode);
+
+    void OP_D(uint16_t opCode);
+
+    void OP_E(uint16_t opCode);
+
+    void OP_F(uint16_t opCode);
 
 };
--- src/Interpreter.cpp
+++ src/Interpreter.cpp
@@ -1,6 +1,102 @@
-﻿#include "Interpreter.h"
+﻿#include <iostream>
+#include "Interpreter.h"
 
 Interpreter::Interpreter(const RomFile &romFile) {
     const std::vector<std::byte> &romData = romFile.GetData();
     std::copy(romData.begin(), romData.end(), memory.begin() + ProgramStartAddress);
+    InitializeMethodInstructions();
+}
+
+void Interpreter::PerformCurrentInstruction() {
+    uint16_t fullOpCode = GetCurrentInstruction();
+    instructionsMethods[static_cast<uint8_t>((fullOpCode & 0xF000) >> 12)](fullOpCode);
+}
+
+uint16_t Interpreter::GetCurrentInstruction() const {
+    return static_cast<uint16_t>(memory[programCounter]) << 8
+           | static_cast<uint16_t>(memory[programCounter + 1]);
+}
+
+void Interpreter::InitializeMethodInstructions() {
+    instructionsMethods = {
+            {0x0, [this](uint16_t opCode) { OP_0(opCode); }},
+            {0x1, [this](uint16_t opCode) { OP_1(opCode); }},
+            {0x2, [this](uint16_t opCode) { OP_2(opCode); }},
+            {0x3, [this](uint16_t opCode) { OP_3(opCode); }},
+            {0x4, [this](uint16_t opCode) { OP_4(opCode); }},
+            {0x5, [this](uint16_t opCode) { OP_5(opCode); }},
+            {0x6, [this](uint16_t opCode) { OP_6(opCode); }},
+            {0x7, [this](uint16_t opCode) { OP_7(opCode); }},
+            {0x8, [this](uint16_t opCode) { OP_8(opCode); }},
+            {0x9, [this](uint16_t opCode) { OP_9(opCode); }},
+            {0xA, [this](uint16_t opCode) { OP_A(opCode); }},
+            {0xB, [this](uint16_t opCode) { OP_B(opCode); }},
+            {0xC, [this](uint16_t opCode) { OP_C(opCode); }},
+            {0xD, [this](uint16_t opCode) { OP_D(opCode); }},
+            {0xE, [this](uint16_t opCode) { OP_E(opCode); }},
+            {0xF, [this](uint16_t opCode) { OP_F(opCode); }},
+    };
+}
+
+void Interpreter::OP_0(uint16_t opCode) {
+}
+
+void Interpreter::OP_1(uint16_t opCode) {
+
+}
+
+void Interpreter::OP_2(uint16_t opCode) {
+
+}
+
+void Interpreter::OP_3(uint16_t opCode) {
+
+}
+
+void Interpreter::OP_4(uint16_t opCode) {
+
+}
+
+void Interpreter::OP_5(uint16_t opCode) {
+
+}
+
+void Interpreter::OP_6(uint16_t opCode) {
+
+}
+
+void Interpreter::OP_7(uint16_t opCode) {
+
+}
+
+void Interpreter::OP_8(uint16_t opCode) {
+
+}
+
+void Interpreter::OP_9(uint16_t opCode) {
+
+}
+
+void Interpreter::OP_A(uint16_t opCode) {
+
+}
+
+void Interpreter::OP_B(uint16_t opCode) {
+
+}
+
+void Interpreter::OP_C(uint16_t opCode) {
+
+}
+
+void Interpreter::OP_D(uint16_t opCode) {
+
+}
+
+void Interpreter::OP_E(uint16_t opCode) {
+
+}
+
+void Interpreter::OP_F(uint16_t opCode) {
+
 }
