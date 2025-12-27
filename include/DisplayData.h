d73b9f3467a01b33fc281f46c740b6aa24db38b1
Jakub Błaszczyk <72347694+liitlelenim@users.noreply.github.com>
2025-12-27 17:53:32 +0100

    Create draw method

--- include/DisplayData.h
+++ include/DisplayData.h
@@ -1,12 +1,15 @@
 ﻿#pragma once
 
+#include <array>
+#include <bitset>
 #include <vector>
-
 class DisplayData {
 public:
     static constexpr unsigned int DisplayWidth = 64;
     static constexpr unsigned int DisplayHeight = 32;
 private:
-
+    std::array<std::bitset<DisplayWidth>, DisplayHeight> displayData{};
 public:
+    //Returns did the collision occur
+    bool Draw(const std::vector<uint8_t> &spriteData, uint8_t xPos, uint8_t yPos);
 };
--- src/DisplayData.cpp
+++ src/DisplayData.cpp
@@ -1,1 +1,22 @@
 ﻿#include "DisplayData.h"
+
+bool DisplayData::Draw(const std::vector<uint8_t> &spriteData, uint8_t xPos, uint8_t yPos) {
+    xPos %= DisplayWidth;
+    yPos %= DisplayHeight;
+    bool collisionHappened = false;
+    for (int y = yPos; y < yPos + spriteData.size(); y++) {
+        for (int x = xPos; x < xPos + 8; x++) {
+            bool initialBitVal = displayData[y][x];
+            displayData[y][x] = displayData[y][x] ^ (0x80 & (spriteData[y - yPos] >> (x - xPos)));
+            if (spriteData[y - yPos] & (0x80 >> (x - xPos))) {
+                displayData[y][x] = !displayData[y][x];
+            }
+            bool disabledPixel = initialBitVal && !displayData[y][x];
+
+            if (disabledPixel) {
+                collisionHappened = true;
+            }
+        }
+    }
+    return collisionHappened;
+}
