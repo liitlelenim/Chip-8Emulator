3cf0945d15d7e2e2d44aa1bebafc704bfab6a3b0
Jakub Błaszczyk <72347694+liitlelenim@users.noreply.github.com>
2025-12-28 01:42:05 +0100

    Create timers class for delay and sound timer handling

---
+++ include/Timers.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+#include <thread>
+
+class Timers {
+public:
+    constexpr static long long RefreshTimeInMilliseconds = 1000 / 60;
+    std::atomic<uint8_t> SoundTimer{};
+    std::atomic<uint8_t> DelayTimer{};
+
+private:
+    std::thread timersThread;
+    std::atomic<bool> isRunning{true};
+public:
+    Timers();
+    ~Timers();
+
+    void KillThread();
+
+private:
+    void CountDownOnTimers();
+};
---
+++ src/Timers.cpp
@@ -0,0 +1,27 @@
+﻿#include <chrono>
+#include <mutex>
+#include "Timers.h"
+
+Timers::Timers() {
+    timersThread = std::thread([this]() { CountDownOnTimers(); });
+};
+
+void Timers::CountDownOnTimers() {
+    while (isRunning) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(RefreshTimeInMilliseconds));
+
+        if (SoundTimer > 0) SoundTimer--;
+        if (DelayTimer > 0) DelayTimer--;
+    }
+}
+
+void Timers::KillThread() {
+    isRunning = false;
+}
+
+Timers::~Timers() {
+    KillThread();
+    if (timersThread.joinable()) {
+        timersThread.join();
+    }
+}
